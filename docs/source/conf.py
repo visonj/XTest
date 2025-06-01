563b399e9ae16db4439098250ed954c729ff261d
He Peiyuan <92845174+visonj@users.noreply.github.com>
2025-06-01 17:00:01 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,8 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'
+html_theme = 'conestack'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
